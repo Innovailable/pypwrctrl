1ed34399eb7f896c29414b6fcc32bbf913413371
Thammi <thammi@chaossource.net>
2013-07-11 16:15:48 +0200

    Add PlugDevice and Plug

--- pypwrctrl/pypwrctrl.py
+++ pypwrctrl/pypwrctrl.py
@@ -4,6 +4,40 @@ import time
 import socket
 import select
 
+class Plug:
+
+    def __init__(self, device, index, name, state=-1):
+        self.device = device
+        self.name = name
+        self.index = index
+        self.state = state
+
+    def switch(self, state):
+        cmd = 'on' if state else 'off'
+        msg = "Sw_{}{}".format(cmd, self.index)
+        self.device._send(msg, True)
+
+class PlugDevice:
+
+    def __init__(self, master, address, name, plug_descs):
+        self.master = master
+        self.name = name
+        self.address = address
+
+        self.plugs = []
+
+        for desc in plug_descs:
+            index, name = desc[0:2]
+            state = desc[2] if len(desc) > 2 else -1
+
+            self.plugs.append(Plug(self, index, name, state))
+
+    def _send(self, data, secured=False):
+        self.master._send(self.address, data, secured)
+
+    def _receive(self, until):
+        return self.master._receive(until, self.address)
+
 class PlugMaster:
 
     def __init__(self, pin=77, pout=74, user="admin", password="anel"):
@@ -18,10 +52,14 @@ class PlugMaster:
         self.sin = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.sin.bind(('0.0.0.0', pin))
 
-    def _send(self, data, address):
-        self.sout.sendto(data, (address, self.pout))
+    def _send(self, address, data, secured=False):
+        if secured:
+            data = data + self.user + self.password
 
-    def _receive(self, until, address=None):
+        print(data)
+        self.sout.sendto(data.encode('utf8'), (address, self.pout))
+
+    def _receive(self, address, until):
         sin = self.sin
 
         while True:
@@ -44,22 +82,22 @@ class PlugMaster:
 
             # right sender?
             if address == None or sender == address:
-                return data
+                return data.decode('utf8')
 
     def discover(self, timeout=1):
         known = set()
         devices = []
 
         # request the answers
-        self._send('wer da?'.encode('utf8'), '255.255.255.255')
+        self._send('255.255.255.255', 'wer da?')
 
         until = time.time() + timeout
 
         while True:
-            data = self._receive(until)
+            data = self._receive(None, until)
 
             if data:
-                parts = data.decode('utf8').strip().split(':')
+                parts = data.strip().split(':')
 
                 name = parts[1]
                 address = parts[2]
@@ -81,7 +119,7 @@ class PlugMaster:
                         name, active = part.rsplit(',', 1)
                         plugs.append((index + 1, name, int(active)))
 
-                device = self.create_device(name, address, plugs)
+                device = self.create_device(address, name, plugs)
 
                 devices.append(device)
             else:
@@ -89,6 +127,6 @@ class PlugMaster:
 
         return devices
 
-    def create_device(self, name, address, plugs):
-        pass
+    def create_device(self, address, name, plugs):
+        return PlugDevice(self, address, name, plugs)
 
